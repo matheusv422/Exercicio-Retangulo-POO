5f7a49aa695c9fd8a2f89f4f7d6b9fe235b7dd69
Matheus Vieira <matheusvr423@gmail.com>
2021-04-03 22:20:17 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicios de Fixacao/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicios_de_Fixacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo r = new Retangulo();
+
+            Console.WriteLine("Entre a largura e altura do retângulo:");
+
+            r.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            r.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            Console.WriteLine(r);
+
+        }
+    }
+}
---
+++ Exercicios de Fixacao/Retangulo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+namespace Exercicios_de_Fixacao
+{
+    class Retangulo
+    {
+        public double Largura, Altura;
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+        public double Perimetro()
+        {
+            return 2 * (Largura + Altura);
+        }
+        public double Diagonal()
+        {
+            return Math.Sqrt(Math.Pow(Largura, 2.0) + Math.Pow(Altura, 2.0));
+        }
+
+        public override string ToString()
+        {
+
+            return "AREA = " + Area()
+                .ToString("F2", CultureInfo.InvariantCulture) + "\n" +
+                "PERIMETRO = " + Perimetro()
+                .ToString("F2", CultureInfo.InvariantCulture) + "\n" +
+                "DIAGONAL = " + Diagonal()
+                .ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+
+}
